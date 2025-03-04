cf0ce52861298757fe9addaea25696ba91737dda
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2025-03-04 23:06:49 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// MongoDB Connection
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
