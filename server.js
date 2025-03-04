69d7383052c4c407c204d5352bed2be899e57abe
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2025-03-04 23:06:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// Connect to the database
 connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
